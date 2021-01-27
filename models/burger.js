6b4f0021e145584b5e1012e0dd4de54244a30bda
Marshall Bertschy <basemarsh@gmail.com>
2021-01-26 19:21:27 -0600

    added burgers model

--- models/burger.js
+++ models/burger.js
@@ -13,7 +13,7 @@ var burger = {
         });
     },
 
-    updateOne: function(table, objColVals, condition, cb) {
+    updateOne: function(objColVals, condition, cb) {
         orm.updateOne("burgers", objColVals, condition, function(res) {
           cb(res);
         });
